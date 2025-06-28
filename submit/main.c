85a93d3cb7fd366d07f2f25993cdec7c3e4ad881
kitsuki <IT.King5963@gmail.com>
2025-06-28 20:30:10 +0900

    add: 標準読み込みのテスト

--- submit/main.c
+++ submit/main.c
@@ -3,17 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
+/*   By: kitsuki <kitsuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 15:37:18 by tookuyam          #+#    #+#             */
-/*   Updated: 2025/06/28 15:45:10 by tookuyam         ###   ########.fr       */
+/*   Updated: 2025/06/28 20:08:26 by kitsuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "hotrace.h"
 
 int	main(void)
 {
-	write(1, "Please implement\n", 17);
+	char	*buf;
+	char	*input;
+	size_t	len;
+
+	buf = NULL;
+	while (true)
+	{
+		input = readline_stdin(&buf);
+		if (input == NULL)
+		{
+			if (buf != NULL)
+				free(buf);
+			return (1);
+		}
+		len = ft_strlen(input);
+		write(STDOUT_FILENO, input, len);
+		if (len != 0 && input[len - 1] != '\n')
+			break ;
+		free(input);
+	}
+	if (buf != NULL)
+		free(buf);
 	return (0);
 }
