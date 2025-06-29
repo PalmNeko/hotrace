6af25fca4fa0266acd80da47931c58bfdb540dba
PalmNeko <nitunamoni13579@gmail.com>
2025-06-29 23:33:20 +0900

    add: argument validation

--- submit/main.c
+++ submit/main.c
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 15:37:18 by tookuyam          #+#    #+#             */
-/*   Updated: 2025/06/29 23:16:21 by tookuyam         ###   ########.fr       */
+/*   Updated: 2025/06/29 23:32:21 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,24 +32,39 @@ static int	handle_error(t_hashmap **map, char **buf, const char *msg)
 	return (1);
 }
 
-int	main(void)
+static int	process_add_values(t_hashmap **map, char **buf)
+{
+	t_input_status	flag;
+
+	while (true)
+	{
+		flag = handle_add_value(map, buf);
+		if (flag == ERROR)
+			return (1);
+		else if (flag == FINISH)
+			break ;
+	}
+	return (0);
+}
+
+int	main(int argc, char *argv[])
 {
 	t_hashmap		*map;
 	char			*buf;
 	t_input_status	flag;
 
+	(void)argv;
+	if (argc != 1)
+	{
+		ft_putstr_fd(STDERR_FILENO, "usage: ./hotrace\n");
+		return (1);
+	}
 	buf = NULL;
 	map = hmap_new_hashmap(877);
 	if (map == NULL)
 		return (handle_error(&map, &buf, "error in: initialize map.\n"));
-	while (true)
-	{
-		flag = handle_add_value(&map, &buf);
-		if (flag == ERROR)
-			return (handle_error(&map, &buf, "error in: add key-value.\n"));
-		else if (flag == FINISH)
-			break ;
-	}
+	if (process_add_values(&map, &buf) != 0)
+		return (handle_error(&map, &buf, "error in: add key-value.\n"));
 	while (true)
 	{
 		flag = handle_get_node(map, &buf);
